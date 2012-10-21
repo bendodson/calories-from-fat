bc03f4b528a71bb40bffd9d9ed21293278ec8bce
Ben Dodson <ben@bendodsonapps.com>
2012-10-21 10:23:20 +0100

    Initial Commit

---
+++ FatCalories/FCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FCAppDelegate.h
+//  FatCalories
+//
+//  Created by Ben Dodson on 21/10/2012.
+//  Copyright (c) 2012 Ben Dodson Apps Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FatCalories/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FatCalories
+//
+//  Created by Ben Dodson on 21/10/2012.
+//  Copyright (c) 2012 Ben Dodson Apps Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FCAppDelegate class]));
+    }
+}
